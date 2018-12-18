a837a7874bc28081aa1920d2c6c13c4e94074d7e
nemo <git@nemo.ikkoku.de>
2018-12-18 23:43:37 +0100

    overspecify page count

--- eventphone/fetch_phonebook.sh
+++ eventphone/fetch_phonebook.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-for i in {1..126}; do
+for i in {1..255}; do
 	curl "https://guru3.eventphone.de/event.rb/phonebook?order=extension&page=$i" | egrep '[ ]{5}[0-9]+'
 done > phonebook.txt
