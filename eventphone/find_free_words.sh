ce6ee8510d92d6525459cb1b97b8da91e6fc6672
nemo <git@nemo.ikkoku.de>
2018-12-24 12:42:00 +0100

    Remove temp files

--- eventphone/find_free_words.sh
+++ eventphone/find_free_words.sh
@@ -4,7 +4,8 @@ pattern_file=$1
 
 bs=1000;
 for i in $(seq 0 $bs $(wc -l $pattern_file | cut -d " " -f 1)); do
-	head -n $(($i + $bs)) $pattern_file | tail -n $bs > tmp_$i;
+	head -n $(($i + $bs)) $pattern_file | tail -n $bs > "tmp_$i"
 	#echo $i $(($i + $bs));
-	egrep -f tmp_$i -i words
+	egrep -f "tmp_$i" -i words
+	rm "tmp_$i"
 done
