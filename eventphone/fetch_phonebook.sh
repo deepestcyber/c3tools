006b4fafa013fce8747c2a3495aa7b08b874c52d
nemo <git@ningu.net>
2023-12-18 16:08:55 +0100

    Update for 37c3

--- eventphone/fetch_phonebook.sh
+++ eventphone/fetch_phonebook.sh
@@ -2,6 +2,6 @@
 for i in {1..255}; do
 	# 35c3
 	#curl "https://guru3.eventphone.de/event.rb/phonebook?order=extension&page=$i" | egrep '[ ]{5}[0-9]+'
-	# 36c3
-	curl 'https://guru3.eventphone.de/event.exe/phonebook?order=extension&page='$i | egrep -o 'tel:[0-9]+' | sed -e 's/tel://'
+	# 36c3, 37c3
+    curl 'https://guru3.eventphone.de/event.exe/phonebook?order=extension&page='$i | egrep -o 'tel:[0-9]+' | sed -e 's/tel://'
 done > phonebook.txt
